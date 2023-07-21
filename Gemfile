b66a19f5ecc0ce11348bfdff250e1b1a2d877481
omairazam <omairr.azam@gmail.com>
2023-07-22 01:17:11 +0500

    chore(specs): integration tests added

--- Gemfile
+++ Gemfile
@@ -89,5 +89,5 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
-  gem "webdrivers"
+  gem 'webdrivers', '~> 5.0', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require 'webdrivers'
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -13,6 +15,7 @@ Capybara.register_driver :selenium do |app|
 end
 
 Capybara.javascript_driver = :selenium
+Webdrivers::Chromedriver.required_version = '114.0.5735.90'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
